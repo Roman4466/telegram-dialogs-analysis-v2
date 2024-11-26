cb4ccad9cf03b97bdb992f5b355ec9463f921645
Roman <91179868+Roman4466@users.noreply.github.com>
2024-11-26 15:26:23 +0200

    d

--- README.md
+++ README.md
@@ -14,5 +14,8 @@ Python 3.8.13
 ```pip install -r requirements.txt```
 
 ### How to start
-0. ```cd ..```
-0. ```jupyter notebook```
+0. Create and fill `config.json` in `config` folder (you may find usefull config_example.json)
+1. Open by Visual Studio Code root of this project
+2. Open `data_analysis_20_questions.ipynb`
+3. Click on `Run all` button (see screehshot)
+![alt text](<Screenshot 2024-11-26 at 15.22.41.png>)
--- download_gifs.py
+++ download_gifs.py
@@ -2,23 +2,20 @@ import os
 import pandas as pd
 import telethon
 from telethon import TelegramClient
-from utils.utils import init_config, read_dialogs
+from utils.utils import init_config
 
-# Load configuration
 config_path = "config/config.json"
 config = init_config(config_path)
 gifs_folder = config["gifs_folder"]
 dialogs_data_folder = config["dialogs_data_folder"]
 
-# Fetch user ID dynamically from config.json
-user_id = int(config["user_id"])  # Ensure it is an integer
+user_id = int(config["user_id"]) 
 full_id = f'PeerUser(user_id={user_id})'
 
 # Ensure the GIFs folder exists
 if not os.path.exists(gifs_folder):
     os.makedirs(gifs_folder)
 
-# Initialize Telegram client
 client = TelegramClient(
     "tmp",
     config["api_id"],
@@ -32,7 +29,6 @@ async def is_gif(message):
     Check if a message is a GIF even if its type is marked as video.
     """
     if message.video:
-        # Check if the file is actually a GIF
         for attribute in message.video.attributes:
             if isinstance(attribute, telethon.tl.types.DocumentAttributeAnimated):
                 return True
@@ -46,14 +42,12 @@ async def download_gifs_from_csv(dialog_file):
     """
     print(f"Processing dialog file: {dialog_file}")
 
-    # Load the CSV file
     try:
         df = pd.read_csv(dialog_file)
     except pd.errors.EmptyDataError:
         print(f"Warning: {dialog_file} is empty or corrupted.")
         return
 
-    # Filter for messages that are videos sent by the user
     video_messages = df[
         (df['type'] == 'video') &
         ((df['from_id'] == user_id) | (df['from_id'] == full_id))
@@ -61,7 +55,6 @@ async def download_gifs_from_csv(dialog_file):
 
     print(f"Found {len(video_messages)} potential GIFs sent by you.")
 
-    # Download each GIF if it's actually a GIF
     for _, row in video_messages.iterrows():
         if pd.notnull(row['id']):
             message_id = row['id']
@@ -71,7 +64,6 @@ async def download_gifs_from_csv(dialog_file):
                 continue
 
             try:
-                # Fetch the actual message from Telegram
                 message = await client.get_messages(entity=row['to_id'], ids=message_id)
                 if await is_gif(message):
                     await client.download_media(message.media, file=file_name)
